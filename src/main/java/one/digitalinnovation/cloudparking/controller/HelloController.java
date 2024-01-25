b00564186aff108fc97190cbbaf8f34021de8b41
Pedro Ferreira <pedrojferreiradev@gmail.com>
2024-01-25 09:04:18 -0300

    test implatação

--- src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String hello(){
         return "hello diioooo eu sou linbdo sgdfsdhgfdhdhgdf" +
                 " ";    }
